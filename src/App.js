f3132eed0edfd207f2d96f22730631912d0e6647
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-06-28 09:47:48 +0100

    added error handling to App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SingleTopicPage from './components/topics/SingleTopicPage';
 import NavBar from './components/navbar/NavBar';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core';
+import ErrorPage from './components/errors/ErrorPage';
 
 
 const theme = createMuiTheme({
@@ -28,10 +29,12 @@ const theme = createMuiTheme({
 
 export default class App extends Component {
   state = {
-    loggedInUser: null
+    loggedInUser: null,
+    error: false,
   }
   render() {
-    const { loggedInUser } = this.state;
+    const { loggedInUser, error } = this.state;
+    if (error) return (<ErrorPage error={error} />)
     return (
       <ThemeProvider theme={theme}>
         <div className="App">
@@ -40,6 +43,8 @@ export default class App extends Component {
             <ArticlesPage loggedInUser={loggedInUser} path="/" />
             <SingleArticlePage loggedInUser={loggedInUser} path="/articles/:article_id" />
             <SingleTopicPage path="/topics/:slug" />
+            {/* if path doesn't match, show error DEFAULT */}
+            <ErrorPage default />
           </Router>
         </div>
       </ThemeProvider>
