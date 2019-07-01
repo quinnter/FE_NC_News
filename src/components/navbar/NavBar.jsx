6a144a8aab1011d38c5247970179bca54a6c0f2e
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-01 15:27:38 +0100

    added more styling to navbar, removed search input

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -31,35 +31,13 @@ const useStyles = makeStyles(theme => ({
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
+        padding: theme.spacing(1, 1, 1, 1),
+        marginLeft: theme.spacing(4),
+        marginRight: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
+            marginRight: theme.spacing(3),
             width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
         },
     },
     sectionDesktop: {
@@ -131,23 +109,12 @@ export default function NavBar({ loginUser, loggedInUser, logoutUser }) {
                             NC News
                     </Typography>
                     </Link>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'Search' }}
-                        />
-                    </div>
-                    <div>
-                        {loggedInUser && <Typography>Welcome {loggedInUser}!</Typography>}
-                    </div>
                     <div className={classes.grow} />
+                    <div>
+                        {loggedInUser &&
+                            <Typography className={classes.search}>Welcome {loggedInUser}!
+                        </Typography>}
+                    </div>
                     <div >
                         <IconButton
                             edge="end"
