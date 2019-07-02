fcba0321b5b3c6abf2385824923584ce2e6db147
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-02 09:36:26 +0100

    added more material ui styling

--- src/components/articles/ArticleCard.jsx
+++ src/components/articles/ArticleCard.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import { Link } from "@reach/router";
-import { withStyles, Grid, IconButton } from '@material-ui/core';
+import { withStyles, Grid, IconButton, Icon, Typography } from '@material-ui/core';
 import ThumbUp from "@material-ui/icons/ThumbUp";
 import ThumbDown from "@material-ui/icons/ThumbDown";
+import Comment from "@material-ui/icons/Comment";
 
 const styles = theme => ({
     border: {
         maxWidth: 900,
-        background: "whitesmoke",
+        // background: "whitesmoke",
         border: 0,
         borderRadius: 5,
         boxShadow: '0 3px 5px 2px lightgrey',
-        padding: "10px",
+        padding: "25px",
         margin: "10px"
     },
     row1: {
         padding: "10px"
     },
     voting: {
-        maxWidth: 120,
+        maxWidth: 115,
+    },
+    comments: {
+        maxWidth: 60,
     },
     body: {
-        background: "white"
+        padding: "10px",
     }
 })
 
@@ -29,26 +33,32 @@ const ArticleCard = ({ article, voteChange = 0, classes, handleVote, onSinglePag
     return (
         <div className={classes.border}>
             <Grid container spacing={2} direction="column">
+                <Grid item container direction="row">
+                    <Typography variant="subtitle2" color="textSecondary">By: {article.author} • {article.created_at} </Typography>
+                </Grid>
                 <Grid item container direction="row" justify="space-between" className={classes.row1}>
                     <Grid item>
                         <Link to={`/articles/${article.article_id}`}>
-                            <h2>{article.title}</h2>
+                            <Typography variant="h5"><strong>{article.title}</strong></Typography>
                         </Link>
-                    </Grid>
-                    <Grid item>
-                        <h3>By: {article.author}</h3>
                     </Grid>
                 </Grid>
                 <Grid item className={classes.body}>
-                    <p>{article.body}</p>
+                    <Typography variant="body1" align="left">{article.body}</Typography>
                 </Grid>
-                <Grid item container direction="row" justify="space-between">
-                    <Grid item>
-                        <h4>Comments: {article.comment_count}</h4>
-                    </Grid>
-                    <Grid item container direction="row" className={classes.voting}>
+                <Grid item container direction="row" justify="space-between" alignItems="center">
+                    <Grid item container direction="row" alignItems="center" className={classes.comments} justify="space-evenly">
                         <Grid item>
-                            <h4>{article.votes + voteChange}</h4>
+                            <Icon><Comment /> </Icon>
+                        </Grid>
+                        <div></div>
+                        <Grid item>
+                            <Typography variant="subtitle2"> {article.comment_count}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item container direction="row" className={classes.voting} alignItems="center" justify="space-evenly">
+                        <Grid item>
+                            <Typography variant="subtitle2">{article.votes + voteChange}</Typography>
                         </Grid>
                         {onSinglePage &&
                             <>
