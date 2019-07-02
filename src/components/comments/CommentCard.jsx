76f82fef1060be57c04f25138cf8bac6d52987c3
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-02 09:02:24 +0100

    added more material ui specific styling

--- src/components/comments/CommentCard.jsx
+++ src/components/comments/CommentCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { withStyles, Grid, IconButton, Divider } from '@material-ui/core';
+import { withStyles, Grid, IconButton, Divider, Typography } from '@material-ui/core';
 import ThumbUp from "@material-ui/icons/ThumbUp";
 import ThumbDown from "@material-ui/icons/ThumbDown";
 import DeleteForever from "@material-ui/icons/DeleteForever"
@@ -22,7 +22,7 @@ const styles = theme => ({
         maxWidth: 120,
     },
     body: {
-        background: "white"
+        align: "left"
     }
 })
 
@@ -36,18 +36,18 @@ class CommentCard extends Component {
         const { comment, loggedInUser, classes } = this.props
         return (
             <div className={classes.border}>
-                <Grid container spacing={2}>
+                <Grid container spacing={2} direction="column">
                     <Grid item container direction="row">
                         <Grid item>
-                            <h3>{comment.author} • {comment.created_at}</h3>
+                            <Typography variant="subtitle1">{comment.author} • {comment.created_at}</Typography>
                         </Grid>
                     </Grid>
                     <Grid item>
-                        <p>{comment.body}</p>
+                        <Typography variant="body2" align="left">{comment.body}</Typography>
                     </Grid>
-                    <Grid item container direction="row">
+                    <Grid item container direction="row" alignItems="center">
                         <Grid item>
-                            <p>{comment.votes + voteChange} •</p>
+                            <Typography variant="body2" color="textSecondary">{comment.votes + voteChange} • </Typography>
                         </Grid>
                         <Grid item>
                             {loggedInUser && <IconButton disabled={voteChange === 1} onClick={() => this.handleVote(1)}> <ThumbUp /> </IconButton>}
@@ -59,11 +59,6 @@ class CommentCard extends Component {
                             {loggedInUser && loggedInUser === comment.author && <Button onClick={() => this.handleDelete(comment.comment_id)}><DeleteForever /> Delete </Button>}
                         </Grid>
                     </Grid>
-
-
-
-
-
                 </Grid>
                 <Divider />
             </div>
