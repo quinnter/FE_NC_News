852c7b6f1acc8f04d20e5fcde264c2c43f2355c3
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-13 16:22:01 +0100

    Remove comments

--- src/components/topics/TopicsDrawer.jsx
+++ src/components/topics/TopicsDrawer.jsx
@@ -1,55 +1,69 @@
-import React, { Component } from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import TopicsList from './TopicsList';
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { Component } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import TopicsList from "./TopicsList";
+import MenuIcon from "@material-ui/icons/Menu";
 import ListAlt from "@material-ui/icons/ListAlt";
-import { getTopics } from '../../api';
-import { IconButton, ListItem, ListItemText, Divider } from '@material-ui/core';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Home from "@material-ui/icons/Home"
-import { Link } from '@reach/router';
+import { getTopics } from "../../api";
+import { IconButton, ListItem, ListItemText, Divider } from "@material-ui/core";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Home from "@material-ui/icons/Home";
+import { Link } from "@reach/router";
 
 export default class TopicsDrawer extends Component {
-    state = {
-        topics: null,
-        left: false,
-        //loading true, when update/mount set to false
-    }
+  state = {
+    topics: null,
+    left: false
+  };
 
-    componentDidMount() {
-        getTopics()
-            .then(topics => {
-                this.setState({ topics })
-            })
-    }
+  componentDidMount() {
+    getTopics().then(topics => {
+      this.setState({ topics });
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <IconButton onClick={this.toggleDrawer('left', !this.state.left)}>
-                    <MenuIcon />
-                </IconButton>
-                <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
-                    <Link to={"/"}>
-                        <ListItem button key="home" onClick={this.toggleDrawer('left', false)}>
-                            <ListItemIcon ><Home /></ListItemIcon>
-                            <ListItemText>Home</ListItemText>
-                        </ListItem>
-                    </Link>
-                    <Divider />
-                    <ListItem>
-                        <ListItemIcon><ListAlt /></ListItemIcon>
-                        <ListItemText ><strong>Topics</strong></ListItemText>
-                    </ListItem>
-                    <Divider />
-                    <TopicsList toggleDrawer={this.toggleDrawer} side={this.state.side} topics={this.state.topics} />
-                </Drawer>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <IconButton onClick={this.toggleDrawer("left", !this.state.left)}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          open={this.state.left}
+          onClose={this.toggleDrawer("left", false)}
+        >
+          <Link to={"/"}>
+            <ListItem
+              button
+              key="home"
+              onClick={this.toggleDrawer("left", false)}
+            >
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </ListItem>
+          </Link>
+          <Divider />
+          <ListItem>
+            <ListItemIcon>
+              <ListAlt />
+            </ListItemIcon>
+            <ListItemText>
+              <strong>Topics</strong>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+          <TopicsList
+            toggleDrawer={this.toggleDrawer}
+            side={this.state.side}
+            topics={this.state.topics}
+          />
+        </Drawer>
+      </div>
+    );
+  }
 
-    toggleDrawer = (side, open) => (event) => {
-        this.setState({ ...this.state, [side]: open });
-    };
-
+  toggleDrawer = (side, open) => event => {
+    this.setState({ ...this.state, [side]: open });
+  };
 }
