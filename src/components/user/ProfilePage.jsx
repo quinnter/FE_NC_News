e14adad2e80bb1983174dc4a2991c4d802716f86
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-13 16:22:54 +0100

    Remove comments

--- src/components/user/ProfilePage.jsx
+++ src/components/user/ProfilePage.jsx
@@ -1,29 +1,24 @@
-import React, { Component } from 'react'
-import { getUser } from '../../api';
-import UserCard from './UserCard';
-
+import React, { Component } from "react";
+import { getUser } from "../../api";
+import UserCard from "./UserCard";
 
 export default class ProfilePage extends Component {
-    state = {
-        user: ''
-    }
+  state = {
+    user: ""
+  };
 
-    componentDidMount() {
-        getUser(this.props.username)
-            .then(user => {
-                this.setState({ user })
-            })
-    }
+  componentDidMount() {
+    getUser(this.props.username).then(user => {
+      this.setState({ user });
+    });
+  }
 
-    render() {
-        const { user } = this.state
-        return (
-            <div>
-                <UserCard user={user} />
-            </div>
-        )
-    }
+  render() {
+    const { user } = this.state;
+    return (
+      <div>
+        <UserCard user={user} />
+      </div>
+    );
+  }
 }
-
-
-// loggedInUser && loggedInUser === user && for when I style this
