539f754407f4efacb32a99a5d84bb4125726525e
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-13 16:12:52 +0100

    Change placeholder value in LoginForm

--- src/components/navbar/LoginForm.jsx
+++ src/components/navbar/LoginForm.jsx
@@ -1,44 +1,40 @@
-import React, { Component } from 'react'
-import { Input, InputLabel, Button, Divider } from '@material-ui/core';
-import { getUser } from '../../api';
-
+import React, { Component } from "react";
+import { Input, InputLabel, Button, Divider } from "@material-ui/core";
+import { getUser } from "../../api";
 
 export default class LoginForm extends Component {
-    state = {
-        userInput: ''
-    }
+  state = {
+    userInput: ""
+  };
 
-    render() {
-        return (
-            <form onSubmit={this.handleSumbit}>
-                <InputLabel>Enter Username</InputLabel>
-                <Input
-                    label="Enter Username"
-                    variant="outlined"
-                    onChange={this.storeUserInput}
-                    value={this.state.userInput}
-                    required
-                ></Input>
-                <Divider />
-                <Button type="submit">Login</Button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSumbit}>
+        <InputLabel>Enter Username</InputLabel>
+        <Input
+          label="jessjelly"
+          variant="outlined"
+          onChange={this.storeUserInput}
+          value={this.state.userInput}
+          required
+        />
+        <Divider />
+        <Button type="submit">Login</Button>
+      </form>
+    );
+  }
 
-    storeUserInput = event => {
-        this.setState({ userInput: event.target.value })
-    }
+  storeUserInput = event => {
+    this.setState({ userInput: event.target.value });
+  };
 
-    handleSumbit = event => {
-        event.preventDefault();
-        const { userInput } = this.state;
-        getUser(userInput)
-            .then(user => {
-                if (!user) return (<p>Invalid UserName</p>)
-                this.setState({ userInput: "" })
-                this.props.loginUser(user.username)
-            })
-    }
-
-
+  handleSumbit = event => {
+    event.preventDefault();
+    const { userInput } = this.state;
+    getUser(userInput).then(user => {
+      if (!user) return <p>Invalid UserName</p>;
+      this.setState({ userInput: "" });
+      this.props.loginUser(user.username);
+    });
+  };
 }
