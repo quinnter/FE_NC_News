a452deab5f38224a0b3cd0a1f9fc5703d40de3db
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-13 16:15:46 +0100

    Remove unused import

--- src/components/articles/ArticlesPage.jsx
+++ src/components/articles/ArticlesPage.jsx
@@ -1,52 +1,47 @@
-import React, { Component } from 'react'
-import { getArticles, sortArticles, orderArticles } from '../../api';
-import ArticlesList from './ArticlesList';
-import SortArticles from './SortArticles';
-// import OrderArticles from './OrderArticles';
-
+import React, { Component } from "react";
+import { getArticles, sortArticles, orderArticles } from "../../api";
+import ArticlesList from "./ArticlesList";
+import SortArticles from "./SortArticles";
 
 export default class ArticlesPage extends Component {
-    state = {
-        articles: null,
-        sortBy: '',
-        order: '',
-    }
-    componentDidMount() {
-        getArticles()
-            .then(articles => {
-                this.setState({ articles: articles })
-            })
-    }
+  state = {
+    articles: null,
+    sortBy: "",
+    order: ""
+  };
+  componentDidMount() {
+    getArticles().then(articles => {
+      this.setState({ articles: articles });
+    });
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.sortBy !== this.state.sortBy) {
-            sortArticles(this.state.sortBy)
-                .then(articles => {
-                    this.setState({ articles })
-                })
-        } else {
-            if (prevState.order !== this.state.order) {
-                orderArticles(this.state.order)
-                    .then(articles => {
-                        this.setState({ articles })
-                    })
-            }
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.sortBy !== this.state.sortBy) {
+      sortArticles(this.state.sortBy).then(articles => {
+        this.setState({ articles });
+      });
+    } else {
+      if (prevState.order !== this.state.order) {
+        orderArticles(this.state.order).then(articles => {
+          this.setState({ articles });
+        });
+      }
     }
+  }
 
-    render() {
-        const { articles, sortBy } = this.state
+  render() {
+    const { articles, sortBy } = this.state;
 
-        return (
-            <div>
-                <SortArticles handleChange={this.handleChange} sortBy={sortBy} />
-                {articles && <ArticlesList articles={articles} />}
-            </div>
-        )
+    return (
+      <div>
+        <SortArticles handleChange={this.handleChange} sortBy={sortBy} />
+        {articles && <ArticlesList articles={articles} />}
+      </div>
+    );
+  }
+  handleChange = event => {
+    if (event.target.value) {
+      this.setState({ [event.target.name]: event.target.value });
     }
-    handleChange = event => {
-        if (event.target.value) {
-            this.setState({ [event.target.name]: event.target.value })
-        }
-    }
+  };
 }
