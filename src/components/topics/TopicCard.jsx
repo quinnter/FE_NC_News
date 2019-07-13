d0b301588fe9cdd7d0300dc4065b55331b6850f8
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-13 16:21:27 +0100

    Remove unused style

--- src/components/topics/TopicCard.jsx
+++ src/components/topics/TopicCard.jsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import { withStyles, Grid } from '@material-ui/core';
+import React from "react";
+import { withStyles, Grid } from "@material-ui/core";
 
 const styles = theme => ({
-    topicCard: {
-        flexGrow: 1,
-        // maxWidth: 260,
-        // height: 250,
-        minWidth: 260,
-        background: theme.palette.primary.light,
-        border: 0,
-        borderRadius: 5,
-        boxShadow: '0 3px 5px 2px lightgrey',
-        margin: "10px"
-    },
-    description: {
-        maxWidth: 150
-    },
-    title: {
-        maxWidth: 150,
-        textTransform: "capitalize",
-    }
-})
+  topicCard: {
+    flexGrow: 1,
+    minWidth: 260,
+    background: theme.palette.primary.light,
+    border: 0,
+    borderRadius: 5,
+    boxShadow: "0 3px 5px 2px lightgrey",
+    margin: "10px"
+  },
+  description: {
+    maxWidth: 150
+  },
+  title: {
+    maxWidth: 150,
+    textTransform: "capitalize"
+  }
+});
 
 function TopicCard({ topic, classes }) {
-    return (
-        <div className={classes.topicCard}>
-            <Grid container spacing={1} direction={"column"} justify="space-evenly" alignItems={"center"}>
-                <Grid item>
-                    <h1 className={classes.title}>Topic: {topic.slug}</h1>
-                </Grid>
-                <h5 className={classes.description}>{topic.description}</h5>
-            </Grid>
-        </div>
-    )
+  return (
+    <div className={classes.topicCard}>
+      <Grid
+        container
+        spacing={1}
+        direction={"column"}
+        justify="space-evenly"
+        alignItems={"center"}
+      >
+        <Grid item>
+          <h1 className={classes.title}>Topic: {topic.slug}</h1>
+        </Grid>
+        <h5 className={classes.description}>{topic.description}</h5>
+      </Grid>
+    </div>
+  );
 }
 
 export default withStyles(styles)(TopicCard);
